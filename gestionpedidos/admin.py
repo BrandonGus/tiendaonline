ced65951ede16a2430b27ce67f25f0271f69d59c
192880 Brandon Hernandez <al192880@alumnos.uacj.mx>
2024-10-19 03:53:42 -0600

    actualizacion

--- gestionpedidos/admin.py
+++ gestionpedidos/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from gestionpedidos.models import client, articulos, pedidos
+
+class clientesadmin(admin.ModelAdmin):
+    list_display=('nombre','direccion','telefono')
+    search_fields=('nombre','direccion','telefono')
+    list_filter=('nombre','direccion','telefono')
+
+class articulosadmin(admin.ModelAdmin):
+    list_display=("nombre",'seccion','precio')
+    list_filter=("nombre",'seccion','precio')
+    search_fields=("nombre",'seccion','precio')
+
+class pedidosadmin(admin.ModelAdmin):
+    list_display=("numero",'fecha','entregado')
+    search_fields=("numero",'fecha','entregado')
+    list_filter=("numero",'fecha','entregado')
+    date_hierarchy="fecha"
+
+admin.site.register(client, clientesadmin)
+admin.site.register(articulos, articulosadmin)
+admin.site.register(pedidos, pedidosadmin)
+
--- gestionpedidos/models.py
+++ gestionpedidos/models.py
@@ -4,9 +4,12 @@ from django.db import models
 
 class client(models.Model):
     nombre=models.CharField(max_length=30)
-    direccion=models.CharField(max_length=50)
-    email=models.EmailField()
+    direccion=models.CharField(max_length=50, verbose_name="La direccion")
+    email=models.EmailField(blank=True, null=True)
     telefono=models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.nombre
 
 class articulos(models.Model):
     nombre=models.CharField(max_length=30)
@@ -14,8 +17,7 @@ class articulos(models.Model):
     precio=models.IntegerField()
 
     def __str__(self):
-        return 'El nombre es %s la seccion es %s y el precio es %s' % (self.nombre, self.seccion, self.precio)
-
+        return self.nombre
 
 class pedidos(models.Model):
     numero=models.IntegerField()
